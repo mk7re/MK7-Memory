621594af11f22a66da2c1ae1f6e438bdef4b4eea
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-05-26 13:47:45 +0200

    Update KDRigid

--- template/KDRigid.hpp
+++ template/KDRigid.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "forward.hpp"
 #include "types.hpp"
+
+#include "KDMtxT.hpp"
+
+#include <math/seadVector.h>
 
 BEGIN_GLOBAL_NAMESPACE
 
@@ -9,6 +14,10 @@ public:
     KDRigid();
     void reset();
 
+    /M/KDMtxT m_kd_mtx/0x30/0x0/ // inherited?
+    /U/sead::Vector4f/0x10/0x30/
+    /M/KDAttT *m_rotation/0x4/0x40/
+    /M/sead::Vector3f *m_position/0x4/0x44/
 /END/
 
 END_GLOBAL_NAMESPACE
