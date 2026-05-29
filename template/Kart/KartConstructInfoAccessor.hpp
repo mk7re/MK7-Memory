c6a2078454c49cc5b65c18f855e2b6a33a7d1ff0
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-05-29 14:00:04 +0200

    Fix namespace

--- template/Kart/KartConstructInfoAccessor.hpp
+++ template/Kart/KartConstructInfoAccessor.hpp
@@ -8,7 +8,7 @@ BEGIN_NAMESPACE(Kart)
 {
 	/START_CLASS/NAME@KartConstructInfoAccessor/SIZE@0x1C/
 	public:
-		/M/Kart::KartPartsConstructParam m_kart_parts_construct_param/0x14/0x0/
+		/M/KartPartsConstructParam m_kart_parts_construct_param/0x14/0x0/
         /M/KartConstructInfoAccessor *m_self/0x4/0x14/ // ?
         /M/Util::ExcelORManager *m_excel_or_manager/0x4/0x18/
 	/END/
