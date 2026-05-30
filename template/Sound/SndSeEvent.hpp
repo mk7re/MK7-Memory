4ae9cee3d8235ea54ce9ef0db5a2e329d60a18bf
Bsquo <suyikaki37@gmail.com>
2026-05-30 12:01:54 +0100

    Small update to ScrollList

--- template/Sound/SndSeEvent.hpp
+++ template/Sound/SndSeEvent.hpp
@@ -16,6 +16,12 @@ BEGIN_NAMESPACE(Sound)
             SE_RC_PAUSE_TO_RESELECT = 0x29,
             SE_PAUSE_TO_REPLAY= 0x2B,
             SE_RC_PAUSE_EXIT_GAME = 0x2C,
+            SE_SYS_DECIDE_M = 0x5A,
+            SE_SYS_BUTTON_INVALID = 0x5B,
+            SE_SYS_NAME_APPEAR = 0x71,
+            SE_SYS_NAME_DISAPPEAR = 0x72,
+            SE_SYS_FRIEND_BTN_TURN_ON = 0x76,
+            SE_SYS_FRIEND_BTN_TURN_OFF = 0x77,
             SE_SYS_DECIDE_M_8A = 0x8A
         };
     /END/
