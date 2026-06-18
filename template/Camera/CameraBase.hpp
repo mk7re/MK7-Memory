2f28fea4ba345bc2ff6d8442398145a70410159a
PabloMK7 <hackyglitch2@gmail.com>
2026-06-18 15:57:36 +0200

    Add CameraBase

---
+++ template/Camera/CameraBase.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../Object/Actor.hpp"
+#include "math/seadVector.h"
+
+BEGIN_NAMESPACE(Camera)
+{
+    /START_CLASS/NAME@CameraBase/SIZE@0x10C/BASE@Object::Actor/BSIZE@0x8/
+    public:
+        /M/float m_zoom/0x4/0x18/ // FOV multiplier? 0-1 range (0 all zoom in)
+        /M/float m_near/0x4/0x24/
+        /M/float m_far/0x4/0x28/
+        /M/sead::Vector3f m_position/0xC/0x2C/
+        /M/sead::Vector3f m_look_at/0xC/0x38/
+        /U/sead::Vector3f/0xC/0x44/
+        /M/bool m_enabled/0x4/0x100/ // Setting to false revents the camera from updating
+        /M/void* m_mw_gfx_camera/0x4/0x108/ // TODO: Implement nw::gfx::Camera
+    /END/
+}
