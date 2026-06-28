97723e360d312c349af9530f7da61d8002203005
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-06-28 10:12:15 +0200

    Add ItemSlotTable indices

--- template/Item/ItemSlotTable.hpp
+++ template/Item/ItemSlotTable.hpp
@@ -12,7 +12,41 @@ BEGIN_NAMESPACE(Item)
     // unused TDirectorBase?
 	/START_CLASS/NAME@ItemSlotTable/SIZE@0xA8/BASE@Object::Actor/BSIZE@0x8/
 	public:
-        /M/sead::FixedPtrArray<CSVUtil::CSVParam, 28> m_param_array/0x7C/0x28/
-        /M/void *m_0xA4/0x4/0xA4/ // RaceSys::ModeManagerBase *m_mode_manager?
+        // NOTE: invented name
+        enum ItemSlotTableIndex
+        {
+            ItemSlotTable_GrandPrix,
+            ItemSlotTable_GrandPrix_AI,
+            ItemSlotTable_VS,
+            ItemSlotTable_VS_AI,
+            ItemSlotTable_WiFi,
+            ItemSlotTable_WiFi_AI,
+            ItemSlotTable_Coin,
+            ItemSlotTable_Coin_AI,
+            ItemSlotTable_Balloon,
+            ItemSlotTable_Balloon_AI,
+            ItemSlotTable_Coin_WiFi,
+            ItemSlotTable_Coin_WiFi_AI,
+            ItemSlotTable_Balloon_WiFi,
+            ItemSlotTable_Balloon_WiFi_AI,
+            ItemSlotTable_Kinoko,
+            ItemSlotTable_Koura,
+            ItemSlotTable_Banana,
+            ItemSlotTable_Bomb,
+            ItemSlotTable_Coin_Kinoko,
+            ItemSlotTable_Coin_Koura,
+            ItemSlotTable_Coin_Banana,
+            ItemSlotTable_Coin_Bomhei,
+            ItemSlotTable_Balloon_Kinoko,
+            ItemSlotTable_Balloon_Koura,
+            ItemSlotTable_Balloon_Banana,
+            ItemSlotTable_Balloon_Bomhei,
+            ItemSlotTable_Decided,
+            ItemSlotTable_Title,
+            ItemSlotTable_MAX,
+        };
+
+        /M/sead::FixedPtrArray<CSVUtil::CSVParam, ItemSlotTable_MAX> m_param_array/0x7C/0x28/
+        /M/RaceSys::ModeManagerBase *m_mode_manager/0x4/0xA4/
 	/END/
 }
