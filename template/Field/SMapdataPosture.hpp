66fa228fff830dad60b28692db0523fe1b6c0818
Bsquo <suyikaki37@gmail.com>
2026-08-09 16:59:43 +0100

    Update SmapdataPosture

--- template/Field/SMapdataPosture.hpp
+++ template/Field/SMapdataPosture.hpp
@@ -7,8 +7,7 @@
 BEGIN_NAMESPACE(Field)
 {
     /START_STRUCT/NAME@SMapdataPosture/SIZE@0x24/
-        // TODO: The symbol indicates that this function has no arguments,
-        // but the assembly suggests it has an additional "SData *" argument?
+        // See https://decomp.me/scratch/dEPBl
         sead::Matrix33f getPosture() const;
 
         /M/sead::Matrix33f m_matrix/0x24/0x0/
