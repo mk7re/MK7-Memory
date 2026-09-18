9246b0f7228a4a3bd1eb2f59a87e3404175d7c58
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-09-18 16:55:35 +0200

    Add `Net::NetworkSelectMenuSendFormat`

---
+++ template/Net/NetworkSelectMenuSendFormat.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../types.hpp"
+
+BEGIN_NAMESPACE(Net)
+{
+    /START_CLASS/NAME@NetworkSelectMenuSendFormat/SIZE@0x9C/
+    public:
+        /M/u8 m_course_id/0x1/0x3/
+        /M/u8 m_driver_id/0x1/0x4/
+        /M/u8 m_body_id/0x1/0x5/
+        /M/u8 m_wing_id/0x1/0x6/
+        /M/u8 m_tire_id/0x1/0x7/
+    /END/
+}
