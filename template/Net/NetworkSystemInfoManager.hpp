30a854b8c033ab72d96bc9804e68679a1be3bf01
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-09-18 18:00:31 +0200

    Add `Net::NetworkSystemInfoManager`

---
+++ template/Net/NetworkSystemInfoManager.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../types.hpp"
+
+#include "../Object/Actor.hpp"
+
+BEGIN_NAMESPACE(Net)
+{
+    /START_CLASS/NAME@NetworkSystemInfoManager/SIZE@0x2F0/BASE@Object::Actor/BSIZE@0x8/
+    public:
+        /M/NetworkSystemInfo m_system_info/0x1F0/0x40/
+    /END/
+}
