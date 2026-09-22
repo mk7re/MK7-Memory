52631439b1caca5b5247010adb4ca38207ee1e54
Anto726 <44021141+Anto726@users.noreply.github.com>
2026-09-22 23:17:13 +0200

    Update SceneManager

---
+++ template/System/GameScene.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../types.hpp"
+
+#include "RootScene.hpp"
+
+BEGIN_NAMESPACE(System)
+{
+    // TODO: GameScene should inherit Scene, so inline getters should probably be pasted in here too
+    /START_CLASS/NAME@GameScene/SIZE@0x310/BASE@RootScene/BSIZE@0x26C/
+    public:
+    /END/
+}
--- template/System/SceneManager.hpp
+++ template/System/SceneManager.hpp
@@ -2,7 +2,7 @@
 
 #include "../types.hpp"
 
-#include "RootScene.hpp"
+#include "GameScene.hpp"
 
 #include "../Render/RenderEngine.hpp"
 #include "../Object/CharacterEngine.hpp"
@@ -15,11 +15,11 @@ BEGIN_NAMESPACE(System)
     public:
         virtual ~SceneManager() = default;
 
-        inline auto get_character_engine_director_list() const { return m_root_scene->get_character_engine()->m_director_list; }
-        inline auto get_render_engine_director_list() const { return m_root_scene->get_render_engine()->m_director_list; }
-        inline auto get_camera_engine_director_list() const { return m_root_scene->get_camera_engine()->m_director_list; }
+        inline auto get_character_engine_director_list() const { return m_game_scene->get_character_engine()->m_director_list; }
+        inline auto get_render_engine_director_list() const { return m_game_scene->get_render_engine()->m_director_list; }
+        inline auto get_camera_engine_director_list() const { return m_game_scene->get_camera_engine()->m_director_list; }
 
-        RootScene *m_root_scene;
+        GameScene *m_game_scene;
     };
     static_assert(sizeof(SceneManager) == 0x8);
 }
