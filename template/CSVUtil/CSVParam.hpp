c7d2371e9d12719560c575ce4728c762bfa13092
Bsquo <suyikaki37@gmail.com>
2026-09-23 13:27:15 +0100

    Update CSVParam

--- template/CSVUtil/CSVParam.hpp
+++ template/CSVUtil/CSVParam.hpp
@@ -18,5 +18,17 @@ BEGIN_NAMESPACE(CSVUtil)
         const sead::Vector3f &getData_vec3(s32, s32) const;
 
         /M/void *m_raw_file/0x4/0x0/
+        // The start of the string data
+        /M/void *m_raw_file_string_data/0x4/0x4/
+        // The start of the max numeric data (floats). Aka `ORRange` in MK8
+        /M/void *m_raw_file_limit_data/0x4/0x8/
+        // The start of the numeric data
+        /M/void *m_raw_file_limit_data/0x4/0xc/
+        /U/s32/0x4/0x10/
+        /U/s32/0x4/0x14/
+        /M/sead::FixedSafeString<32> m_directory/0x2c/0x18/
+        /M/sead::FixedSafeString<32> m_filename/0x2c/0x44/
+        /M/sead::FixedSafeString<32> m_directory_2/0x2c/0x70/
+        /M/sead::FixedSafeString<128> m_include_directory/0x8c/0x9c/
 	/END/
 }
